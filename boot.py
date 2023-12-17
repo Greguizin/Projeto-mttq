2f00fee6f5fd8897686f03967c1d069d9559e009
Greguizin <154081525+Greguizin@users.noreply.github.com>
2023-12-17 14:09:36 -0300

    Add files via upload

---
+++ boot.py
@@ -0,0 +1,62 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+import time
+from umqttsimple import MQTTClient
+import ubinascii
+import micropython
+import network
+import esp
+esp.osdebug(None)
+import gc
+gc.collect()
+import machine, neopixel
+import time
+from machine import Pin, ADC
+import json 
+conf_file = open('conf.json.example') 
+conf = json.load(conf_file) 
+conf_file.close()
+
+cores = ['verde','vermelho','azul']
+n = 1
+np = neopixel.NeoPixel(machine.Pin(23), n)
+
+ssid = conf["ssid"]
+password = conf["password"]
+mqtt_server = conf["mqtt_server"]
+server_port = conf["server_port"] 
+mqtt_user = conf["mqtt_user"]
+mqtt_password = conf["mqtt_password"]
+
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_pub = b'test_mqtt/output'
+ 
+# For the publish  topic 
+counter = 0 
+last_message = time.time()
+message_interval = 0.1 
+x = 0
+ldr = ADC(Pin(33))
+
+
+station = network.WLAN(network.STA_IF)
+station.active(True)
+
+if not station.isconnected():
+
+  print('Connecting to Wi-Fi...')
+
+  station.connect(ssid, password)
+
+  while not station.isconnected():
+    pass
+
+  print('Connected to Wi-Fi: ', station.ifconfig())
+
+  
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,63 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+class Leitura:
+    def __init__(self, faixa, cor):
+        self.faixa = faixa
+        self.cor = cor
+        
+def ldrler():
+    num_leituras = 5
+    ldr_value = sum(ldr.read() for _ in range(num_leituras))
+    ldr.atten(ADC.ATTN_11DB)
+    return ldr_value / num_leituras
+
+def rgbcor(z):
+    cores = {
+        'azul': (0, 0, 255),
+        'vermelho': (255, 0, 0),
+        'verde': (0, 255, 0)
+    }
+    cor = cores.get(z)
+    if cor is not None:
+        np[0] = cor
+        np.write()
+ldrler()
+
+ 
+def client_connect():
+  global client_id, mqtt_server, topic_sub, server_port, mqtt_user, mqtt_password
+  client = MQTTClient(client_id, mqtt_server, server_port, mqtt_user, mqtt_password)
+  client.connect()
+  print('Connected to %s MQTT broker!' % (mqtt_server))
+  return client
+
+def restart_and_reconnect():
+  print('Failed to connect to MQTT broker. Reconnecting...')
+  time.sleep(10)
+  machine.reset()
+
+try:
+  client = client_connect()
+except OSError as e:
+  restart_and_reconnect()
+
+while True:
+  try:
+    if (time.time() - last_message) > message_interval:
+      # write on output topic
+        for cor in cores:
+            rgbcor(cor)
+            time.sleep(5)
+            valor_lido = ldrler()
+            teste = Leitura(valor_lido, cor)
+            print(teste.faixa, teste.cor)
+            msg = b' a cor %s marcou : %d ' % (teste.cor, teste.faixa)
+            client.publish(topic_pub, msg)
+        last_message = time.time()
+        counter += message_interval    
+  except OSError as e:
+        restart_and_reconnect()
+
+
+
+
